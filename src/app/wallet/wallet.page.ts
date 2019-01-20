cda3592c472e428dbe8f0e1f6eb0534efc2ba8bc
calidion <calidion@gmail.com>
2019-01-20 11:20:53 +0800

    updated windows wallet

--- src/app/wallet/wallet.page.ts
+++ src/app/wallet/wallet.page.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['wallet.page.scss']
 })
 export class WalletPage {
-  WindowsURL = "https://github.com/vigcoin/wallet/releases/download/1.0.0-b1/vigcoin-installer-v1.0.exe";
+  WindowsURL = "https://github.com/vigcoin/wallet/releases/download/1.0.0-b1/vigcoin-wallet-installer-v1.0.0-b2.msi";
   LinuxURL = "https://github.com/vigcoin/wallet/releases/download/1.0.0-b1/VIGCOIN-x86_64.AppImage";
 }
